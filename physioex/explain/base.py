0013d81765b49fa097f3cfcb6438390fdb4146b3
e-cremente <155981951+e-cremente@users.noreply.github.com>
2024-01-15 22:11:52 +0000

    .

--- physioex/explain/base.py
+++ physioex/explain/base.py
@@ -58,7 +58,7 @@ class PhysioExplainer(ABC):
                 self.checkpoints[fold] = elem.path
 
         logger.info("Found %d checkpoints" % len(self.checkpoints))
-        logger.debug("fold = %d, %d", fold, int(fold))
+        logger.debug("fold = %d, %d", %fold, %int(fold))
 
         self.ckpt_path = ckp_path
 
