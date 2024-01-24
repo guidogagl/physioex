4fc43c97ab99cbd557b1046c49976fd46e698881
e-cremente <155981951+e-cremente@users.noreply.github.com>
2024-01-24 14:57:33 +0000

    .

--- physioex/explain/freq_bands_explainer.py
+++ physioex/explain/freq_bands_explainer.py
@@ -27,10 +27,11 @@ from physioex.data import datasets, TimeDistributedModule
 from loguru import logger
 from tqdm import tqdm
 
+from torch.utils import data as D
 from torch.nn import functional as F
 torch.set_float32_matmul_precision('medium')
 
-def compute_band_importance(bands : list[list[float]], band_names : list[str], model, dataloader : torch.nn.Module, model_device : torch.device, sampling_rate: int = 100):
+def compute_band_importance(bands : list[list[float]], model : torch.nn.Module, dataloader : D.DataLoader , model_device : torch.device, sampling_rate: int = 100):
     y_pred = []
     y_true = []
     importance = []
@@ -63,17 +64,18 @@ def compute_band_importance(bands : list[list[float]], band_names : list[str], m
         # now inputs size is batch_size * (seq_len * n_channels (1) * n_samples)
         # remove the frequency band from the input using scipy
         
-        # filter bandstop - reject the frequencies specified in freq_band
-        lowcut = bands[current_band][0]
-        highcut = bands[current_band][1]
-        order = 4
-        nyq = 0.5 * sampling_rate
-        low = lowcut / nyq
-        high = highcut / nyq
-        sos = signal.butter(order, [low, high], btype='bandstop', output='sos')
+        for band in bands:
+            # filter bandstop - reject the frequencies specified in freq_band
+            lowcut = bands[band][0]
+            highcut = bands[band][1]
+            order = 4
+            nyq = 0.5 * sampling_rate
+            low = lowcut / nyq
+            high = highcut / nyq
+            sos = signal.butter(order, [low, high], btype='bandstop', output='sos')
 
-        for index in range(batch_size):     
-            inputs[index] = signal.sosfilt(sos, inputs[index])
+            for index in range(batch_size):     
+                inputs[index] = signal.sosfilt(sos, inputs[index])
 
         # reshape the input signal to the original size and port it to tensor
         inputs = inputs.reshape(batch_size, seq_len, n_channels, n_samples)
@@ -93,6 +95,34 @@ def compute_band_importance(bands : list[list[float]], band_names : list[str], m
     importance = np.concatenate(importance).reshape(-1, n_class)
 
     return importance, y_pred, y_true
+
+def get_normalized_weights(lenght : int):
+    weights = []
+    sum = 0
+
+    for i in range(lenght):
+        weights.append(1/(i+1))
+        sum += 1/(i+1)
+
+    weights = np.array(weights)
+    weights = weights / sum
+
+    return weights
+
+def get_band_importance(band : str, band_dict : dict):
+    shape = 0
+    counter = 0
+    for key, value in band_dict:
+        if shape == 0:
+            importance = np.zeros(value.shape)
+            shape = 1
+        if band in key:
+            importance += value
+            counter += 1
+    
+    importance = importance / counter
+
+    return importance
 
 class FreqBandsExplainer(PhysioExplainer):
     def __init__(self,
@@ -134,58 +164,92 @@ class FreqBandsExplainer(PhysioExplainer):
 
         self.module_config["loss_params"]["class_weights"] = datamodule.class_weights()
 
-        importance, y_pred, y_true = compute_band_importance(bands, band_names, model, datamodule.train_dataloader(), model_device, self.sampling_rate)
+        #la band_importance e' un dict che ha come chiavi le combinazioni delle bande, e come valori l'importanza per quella combinazione
+        band_importance = {}
+
+        #combinations e' la lista in cui finiranno le varie combinazioni. in particolare e' una lista di liste. ogni elemento della lista e' una
+        #lista di combinazioni di bande. il primo elemento e' la lista di combinazioni di 1 banda, il secondo elemento e' la lista di combinazioni di 2 bande, e cosi' via
+        band_names_combinations = []
+        band_freq_combinations = []
+
+        #pesi normalizzati in base al numero di bande da filtrare
+        #weights = get_normalized_weights(len(bands))
+
+        for i in range(len(bands)):
+            band_names_combinations.append(list(it.combinations(band_names, i+1)))
+            band_freq_combinations.append(list(it.combinations(bands, i+1)))
+
+        #ora, per ogni combinazione possibile di bande, mi calcolo l'importanza e la metto in band_importance, associata alla relativa chiave
+        for i in range(len(band_freq_combinations)):
+            for j in range(len(band_freq_combinations[i])):
+                bands_set = list(band_freq_combinations[i][j])
+                importance, y_pred, y_true = compute_band_importance(bands_set, model, datamodule.train_dataloader(), model_device, self.sampling_rate)
+                #da moltiplicare per weights[i] per media pesata
+                band_importance[str(list(band_names_combinations[i][j]))] = importance
+
+        #ora band_importance e' un dizionario come descritto piu' su                
         
-        if plot_true:
-            # boxplot of the band importance of the true label
-            logger.info("JOB:%d-Plotting band importance for true label" % fold)
-            true_importance = []
-            
-            for i in range(len(y_true)):
-                true_importance.append(importance[i][y_true[i]])
-            
-            true_importance = np.array(true_importance)
+        result = []
+        for band in band_names:
 
-            df = pd.DataFrame({
-                'Band ' + band_names[current_band] + ' Importance': true_importance,
-                'Class': y_true
-            })
+            #in base alla banda, ora dovro' prendermi l'importanza di quella banda per poterla plottare. per farlo, devo prendere, dal mio dizionario,
+            #tutte le importanze in cui la mia banda compare, e poi farne la media
+            importance = get_band_importance(str(band), band_importance)
 
-            # boxplot of the true importance of the band with seaborn
-            plt.figure(figsize=(10, 10))
-            ax = sns.boxplot(x='Class', y='Band ' + band_names[current_band] + ' Importance', data=df)
-            ax.set_xticklabels(self.class_name)
-            plt.title('Band ' + band_names[current_band] + ' Importance for True Label (freq. ' + str(bands[current_band]) +')')
-            plt.xlabel('Class')
-            plt.ylabel('Importance')
-            plt.savefig(self.ckpt_path + ("fold=%d_true_band=" + band_names[current_band] + "_importance.png") % fold)
-            plt.close()
+            if plot_true:
+                # boxplot of the band importance of the true label
+                logger.info("JOB:%d-Plotting band %s importance for true label" % (fold, band))
+                true_importance = []
+                
+                for i in range(len(y_true)):
+                    true_importance.append(importance[i][y_true[i]])
+                
+                true_importance = np.array(true_importance)
 
-        if plot_pred:
-            logger.info("JOB:%d-Plotting band importance for predicted label" % fold)
-            pred_importance = []
-            
-            for i in range(len(y_true)):
-                pred_importance.append(importance[i][y_pred[i]])
-            
-            pred_importance = np.array(pred_importance)
+                df = pd.DataFrame({
+                    'Band ' + band + ' Importance': true_importance,
+                    'Class': y_true
+                })
 
-            df = pd.DataFrame({
-                'Band ' + band_names[current_band] + ' Importance': pred_importance,
-                'Class': y_true
-            })
+                # boxplot of the true importance of the band with seaborn
+                plt.figure(figsize=(10, 10))
+                ax = sns.boxplot(x='Class', y='Band ' + band + ' Importance', data=df)
+                ax.set_xticklabels(self.class_name)
+                plt.title('Band ' + band + ' Importance for True Label')
+                plt.xlabel('Class')
+                plt.ylabel('Importance')
+                plt.savefig(self.ckpt_path + ("fold=%d_true_band=" + band + "_importance.png") % fold)
+                plt.close()
 
-            # boxplot of the true importance of the band with seaborn
-            plt.figure(figsize=(10, 10))
-            ax = sns.boxplot(x='Class', y='Band ' + band_names[current_band] + ' Importance', data=df)
-            ax.set_xticklabels(self.class_name)
-            plt.title('Band ' + band_names[current_band] + ' Importance for Predicted Label (freq. ' + str(bands[current_band]) +')')
-            plt.xlabel('Class')
-            plt.ylabel('Importance')
-            plt.savefig(self.ckpt_path + ("fold=%d_pred_band=" + band_names[current_band] + "_importance.png") % fold)
-            plt.close()
-        
-        result = np.column_stack([importance, y_pred, y_true])
+            if plot_pred:
+                logger.info("JOB:%d-Plotting band %s importance for predicted label" % (fold, band))
+                pred_importance = []
+                
+                for i in range(len(y_true)):
+                    pred_importance.append(importance[i][y_pred[i]])
+                
+                pred_importance = np.array(pred_importance)
+
+                df = pd.DataFrame({
+                    'Band ' + band + ' Importance': pred_importance,
+                    'Class': y_true
+                })
+
+                # boxplot of the true importance of the band with seaborn
+                plt.figure(figsize=(10, 10))
+                ax = sns.boxplot(x='Class', y='Band ' + band + ' Importance', data=df)
+                ax.set_xticklabels(self.class_name)
+                plt.title('Band ' + band + ' Importance for Predicted Label')
+                plt.xlabel('Class')
+                plt.ylabel('Importance')
+                plt.savefig(self.ckpt_path + ("fold=%d_pred_band=" + band + "_importance.png") % fold)
+                plt.close()
+
+                #result prima era una matrice che aveva, per ogni riga, l'importanza di una banda per ogni classe, affiancata da y_pred e y_true
+                #adesso lo rendiamo un array di matrici, ogni posizione dell'array corrisponde a una banda
+                #l'array viene inizializzato prima del for, come vuoto
+                result.append(np.column_stack([importance, y_pred, y_true]))
+
         return result
     
     def explain(self, bands : list[list[float]], band_names : list[str], save_csv : bool = False, plot_pred : bool = False, plot_true : bool = False, n_jobs : int = 10):
@@ -196,22 +260,19 @@ class FreqBandsExplainer(PhysioExplainer):
 
         # Converte i risultati in una matrice numpy
         results = np.array(results, dtype=object)
-        
-        df = pd.DataFrame([])
 
-        #questo codice andava bene finche' la explain descriveva una sola banda e veniva chiamata in un ciclo. ora non va piu' bene, per lo meno non piu' qui.
-        #valutare se spostare questa sezione all'interno della compute band importance per poter continuare a generare i file con i risultati
+        for i in range(len(band_names)):
+            df = pd.DataFrame([])
 
-        #for fold in self.checkpoints.keys():
-        #    df = df.append(pd.DataFrame({
-        #        "Band Importance": results[fold][:, :-2].tolist(),
-        #        "Predicted Label": results[fold][:, -2],
-        #        "True Label": results[fold][:, -1],
-        #        "Fold": int(fold)
-        #    }))
+            for fold in self.checkpoints.keys():
+                df = df.append(pd.DataFrame({
+                   "Band Importance": results[i][fold][:, :-2].tolist(),
+                    "Predicted Label": results[i][fold][:, -2],
+                    "True Label": results[i][fold][:, -1],
+                    "Fold": int(fold)
+                }))
 
-        #valutare se aggiungere questa riga all'interno di compute band importance passandogli anche come parametro il save_csv
-        #if save_csv:
-        #    df.to_csv(self.ckpt_path + "band=" + band_names[current_band] + "_importance.csv", index=False)
-            
+            if save_csv:
+                df.to_csv(self.ckpt_path + "band=" + band_names[i] + "_importance.csv", index=False)
+               
         return df
