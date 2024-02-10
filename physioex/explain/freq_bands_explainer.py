adc8eef8055c2c7f2a272a35cf1421772bfedc86
e-cremente <155981951+e-cremente@users.noreply.github.com>
2024-02-10 17:06:45 +0000

    .

--- physioex/explain/freq_bands_explainer.py
+++ physioex/explain/freq_bands_explainer.py
@@ -113,6 +113,7 @@ def _compute_cross_band_importance(bands : List[List[float]], model : torch.nn.M
         if partial_time_importance.shape == (5, 32, 3, 1, 3000):
             print("sono qui")
             time_importance.append(partial_time_importance)
+            print(partial_time_importance[0][0][0][0])
         
     time_importance = np.array(time_importance)
     print("shape of time importance")
