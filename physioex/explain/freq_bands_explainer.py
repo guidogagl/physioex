2df4fbeb4cd307824f62f08e43c2ba7f09f8b0d4
e-cremente <155981951+e-cremente@users.noreply.github.com>
2024-02-12 19:14:17 +0000

    .

--- physioex/explain/freq_bands_explainer.py
+++ physioex/explain/freq_bands_explainer.py
@@ -128,7 +128,7 @@ def _compute_cross_band_importance(bands : List[List[float]], model : torch.nn.M
     return time_importance, band_importance, y_pred, y_true
 
 #RICORDA DI LEVARE I PRIMI DUE PARAMETRI
-def compute_band_importance(path, fold, bands : List[List[float]], band_names: List[str],  model : torch.nn.Module, dataloader : DataLoader , model_device : torch.device, sampling_rate: int = 100, class_names : list[str] = ["Wake", "N1", "N2", "DS", "REM"], average_type : int = 0):
+def compute_band_importance(bands : List[List[float]], band_names: List[str],  model : torch.nn.Module, dataloader : DataLoader , model_device : torch.device, sampling_rate: int = 100, class_names : list[str] = ["Wake", "N1", "N2", "DS", "REM"], average_type : int = 0):
     
     for i in range(len(bands)):
         assert len(bands[i]) == 2
@@ -154,6 +154,7 @@ def compute_band_importance(path, fold, bands : List[List[float]], band_names: L
     #lista di combinazioni di bande. il primo elemento e' la lista di combinazioni di 1 banda, il secondo elemento e' la lista di combinazioni di 2 bande, e cosi' via
     band_freq_combinations = []
     band_combinations_dict = {}
+    band_time_combinations_dict = {}
 
     for i in range(len(bands)):
         combination_list = it.combinations(bands, i+1)
@@ -177,27 +178,16 @@ def compute_band_importance(path, fold, bands : List[List[float]], band_names: L
                 permuted_bands [i] = 1
         
         print(permuted_bands)
-        _, band_importance, y_pred, y_true = _compute_cross_band_importance(cross_band, model, dataloader, model_device, sampling_rate)
+        time_importance, band_importance, y_pred, y_true = _compute_cross_band_importance(cross_band, model, dataloader, model_device, sampling_rate)
 
         band_combinations_dict[str(permuted_bands)] = band_importance
-            #al posto di un dataframe qui mi popolo un dict con le combinazioni delle bande
-
-#            importance_df = pd.DataFrame( band_importance, columns = class_names )
-
-#            importance_df.insert(0, "Sample", range(0, 0 + len(importance_df)))
-#            importance_df["y_pred"] = y_pred
-#            importance_df["y_true"] = y_true
-#            for i, band in enumerate(band_names):
-#                importance_df[band] = permuted_bands[i] * np.ones( len(y_pred) )
-    
-#            importances_df.append( importance_df )
-    
-#        importances_df = pd.concat( importances_df )
+        band_time_combinations_dict[str(permuted_bands)] = time_importance
 
     permuted_bands_importance = []
+    permuted_bands_time_importance = []
 
     # Genera tutte le possibili permutazioni di 0 e 1 lungo 6
-    permutations = list(it.product([0, 1], repeat=6))
+    permutations = list(it.product([0, 1], repeat=len(bands)))
     # Filtra le permutazioni escludendo quelle con tutti 0
     filtered_permutations = [p for p in permutations if any(x == 1 for x in p)]
     # Converte la lista di tuple in un array NumPy
@@ -206,8 +196,10 @@ def compute_band_importance(path, fold, bands : List[List[float]], band_names: L
     for i in range(len(permutations_array)):
         key = permutations_array[i]
         permuted_bands_importance.append(band_combinations_dict[str(key)])
+        permuted_bands_time_importance.append(band_time_combinations_dict[str(key)])
 
     importances_matrix = []
+    time_importances_matrix = []
 
     for i in range(len(bands)):
         #calcolare importanza della banda band per ogni sample
@@ -215,13 +207,16 @@ def compute_band_importance(path, fold, bands : List[List[float]], band_names: L
         #simple_average
         if average_type == 0:
             band_importance = get_simple_importance(permuted_bands_importance, permutations_array, i)
+            band_time_importance = get_simple_importance(permuted_bands_time_importance, permutations_array, i)
         #weighted_average
         elif average_type == 1:
             band_importance = get_weighted_importance(permuted_bands_importance, permutations_array, i)
+            band_time_importance = get_weighted_importance(permuted_bands_time_importance, permutations_array, i)
 
         importances_matrix.append(band_importance)
+        time_importances_matrix.append(band_time_importance)
 
-    return importances_matrix, y_pred, y_true
+    return time_importances_matrix, importances_matrix, y_pred, y_true
 
 def get_simple_importance(permuted_bands_importance : List[np.ndarray], permutations_array : List[List[int]], band : int = 0):
         importance = np.zeros(permuted_bands_importance[0].shape)
@@ -275,147 +270,7 @@ class FreqBandsExplainer(PhysioExplainer):
         self.sampling_rate = sampling_rate
         self.class_name = class_name
 
-    def compute_band_time_importance(self, bands : List[List[float]], band_names : List[List[str]], model : torch.nn.Module, dataloader : DataLoader, model_device : torch.device, sampling_rate: int = 100, class_names : list[str] = ["Wake", "N1", "N2", "DS", "REM"], average_type : int = 0, target_band : int = 0, target_class : int = 0):
-        
-#        dataloader = torch.utils.data.DataLoader(
-#            dataloader.dataset,
-#            batch_size=dataloader.batch_size,
-#            shuffle=False,
-#            num_workers=dataloader.num_workers,
-#            collate_fn=dataloader.collate_fn,
-#            pin_memory=dataloader.pin_memory,
-#            drop_last=dataloader.drop_last,
-#            timeout=dataloader.timeout,
-#            worker_init_fn=dataloader.worker_init_fn,
-#        )
-        logger.info("Plotting time importance of target band %s for target class %s" % (band_names[target_band], class_names[target_class]))
-
-        found = False
-        for batch in dataloader:
-            y_true = []
-            inputs, y_true_batch = batch
-            nd_inputs, nd_y_true_batch = batch
-            y_true.append(y_true_batch.numpy())
-            y_true = np.concatenate(y_true).reshape(-1)
-
-            # port the input to numpy
-            inputs = inputs.cpu().detach().numpy()
-            batch_size, seq_len, n_channels, n_samples = inputs.shape
-    
-            for i in range(batch_size):           
-                if y_true[i] != target_class:
-                    continue
-                found = True
-                index = i
-                inputs = inputs[i].reshape(seq_len, n_samples)
-                #creo un nuovo DataLoader con il primo batch che contenga la classe target
-                new_dataloader = DataLoader(
-                    TensorDataset(nd_inputs, nd_y_true_batch),
-                    batch_size=dataloader.batch_size,
-                    shuffle=False,
-                    num_workers=dataloader.num_workers,
-                    collate_fn=dataloader.collate_fn,
-                    pin_memory=dataloader.pin_memory,
-                    drop_last=dataloader.drop_last,
-                    timeout=dataloader.timeout,
-                    worker_init_fn=dataloader.worker_init_fn
-                )
-
-                if found == True:
-                    break
-            
-            if found == True:
-                break
-                
-        band_freq_combinations = []
-        target_band_time_cross_importance = []
-        filtered_permutations = []
-
-        for i in range(len(bands)):
-            combination_list = it.combinations(bands, i+1)
-            for elem in combination_list:
-                band_freq_combinations.append(elem)    
-
-        for cross_band in band_freq_combinations:
-            if bands[target_band] not in cross_band:
-                continue
-
-            permuted_bands = np.zeros( len( bands ) )
-
-            for i, band in enumerate( bands ):
-                if band in cross_band:
-                    permuted_bands [i] = 1
-
-            filtered_permutations.append(permuted_bands)
-
-            time_importance, _, y_pred, _ = _compute_cross_band_importance(cross_band, model, new_dataloader, model_device, sampling_rate)
-
-            time_importance = np.array(time_importance)
-#            print(time_importance.shape)
-    
-            target_band_time_cross_importance.append(time_importance)
-
-        permutations_array = np.array(filtered_permutations)
-        target_band_time_cross_importance = np.array(target_band_time_cross_importance)
-#        print("shape di target_band_time_cross_importance")
-#        print(target_band_time_cross_importance.shape)
-#        print("shape di permutations_array")
-#        print(permutations_array.shape)
-#        print("contenuto di permutations array")
-#        print(permutations_array)
-
-        if average_type == 0:
-            target_band_time_importance = get_simple_importance(target_band_time_cross_importance, permutations_array, target_band)
-            word = "simple"
-        elif average_type == 1:
-            target_band_time_importance = get_weighted_importance(target_band_time_cross_importance, permutations_array, target_band)
-            word = "weighted"
-
-        #la dimensione di target_band_time_importance è batch_size, seq_len, n_samples, n_class
-        #per plottare, provo a plottare solamente la matrice di shape seq_len, n_samples, n_class
-
-        #plot_matrix = target_band_time_importance[index]
-            
-        plot_matrix = target_band_time_importance[target_class][index]
-            
-        #num_batch, seq_len, n_samples, n_class = target_band_time_importance.shape
-                   
-#        plot_matrix = target_band_time_importance[0]
-
-        for i in range(seq_len):
-            y = []
-            for j in range(n_samples):
-                y.append(plot_matrix[i][j])
-
-            x = np.arange(n_samples)
-
-            plt.figure(figsize=(15, 5))
-            plt.subplots_adjust(hspace=0.5)
-
-            plt.subplot(2, 1, 1)           
-            plt.plot(x, y)
-            plt.title("Band " + band_names[target_band] + " time " + word + " importance for the samples of the sequence " + str(i+1) + " of the first instance of target class " + class_names[target_class])
-
-            plt.ylabel('Time Importance')
-
-            x = np.arange(n_samples)
-            y = inputs[i]
-
-            plt.subplot(2, 1, 2)
-            plt.plot(x, y)
-            plt.title("Original corresponding input wave")
-
-            plt.ylabel("Wave value")
-            plt.xlabel("Samples")
-
-            plt.savefig(self.ckpt_path + "num_seq=" + str(i) + "_target_band=" + band_names[target_band] + "_target_class=" + class_names[target_class] + "_" + word + "_time_importance.png")
-            plt.close()
-
-        logger.info("Done plotting")
-
-        return target_band_time_importance
-
-    def compute_band_importance(self, bands : List[List[float]], band_names : List[str], fold : int = 0, plot_pred : bool = False, plot_true : bool = False, save_csv : bool = False):
+    def compute_band_importance(self, bands : List[List[float]], band_names : List[str], fold : int = 0, plot_pred : bool = False, plot_true : bool = False, save_csv : bool = False,  pred_or_true : int = 0):
         logger.info("JOB:%d-Loading model %s from checkpoint %s" % (fold, str(self.model_call), self.checkpoints[fold]))
         model = self.model_call.load_from_checkpoint(self.checkpoints[fold], module_config = self.module_config).eval()
 
@@ -434,11 +289,13 @@ class FreqBandsExplainer(PhysioExplainer):
 
         self.module_config["loss_params"]["class_weights"] = datamodule.class_weights()
 
-        target_band_time_importance = self.compute_band_time_importance(bands, band_names, model, datamodule.train_dataloader(), model_device, self.sampling_rate, self.class_name, 0, 1, 2)
+        dataloader = datamodule.train_dataloader()
+
+#        target_band_time_importance = self.compute_band_time_importance(bands, band_names, model, datamodule.train_dataloader(), model_device, self.sampling_rate, self.class_name, 0, -1)
 
         for i in range(2):
             # RICORDA DI LEVARE I PRIMI DUE PARAMETRI 
-            matrixes_importance, y_pred, y_true = compute_band_importance(self.ckpt_path, str(fold), bands, band_names, model, datamodule.train_dataloader(), model_device, self.sampling_rate, self.class_name, i)
+            time_importances_matrix, importances_matrix, y_pred, y_true = compute_band_importance(bands, band_names, model, dataloader, model_device, self.sampling_rate, self.class_name, i)
 
             if i == 0:
                 word = "simple"
@@ -455,7 +312,7 @@ class FreqBandsExplainer(PhysioExplainer):
                     true_importance = []
                 
                     for k in range(len(y_true)):
-                        true_importance.append(matrixes_importance[j][k][y_true[k]])
+                        true_importance.append(importances_matrix[j][k][y_true[k]])
                     
                     true_importance = np.array(true_importance)
 
@@ -481,7 +338,7 @@ class FreqBandsExplainer(PhysioExplainer):
                     pred_importance = []
                     
                     for k in range(len(y_true)):
-                        pred_importance.append(matrixes_importance[j][k][y_pred[k]])
+                        pred_importance.append(importances_matrix[j][k][y_pred[k]])
                     
                     pred_importance = np.array(pred_importance)
 
@@ -500,7 +357,64 @@ class FreqBandsExplainer(PhysioExplainer):
                     plt.savefig(self.ckpt_path + ("fold=%d_pred_band=" + band + "_" + word + "_importance.png") % fold)
                     plt.close()
 
-                df_current_average = pd.DataFrame(matrixes_importance[j], columns = self.class_name)
+            for k, class_name in enumerate(self.class_name):
+                logger.info("JOB:%d-Plotting time importance of target band %s for target class %s" % (fold, band, class_name))
+
+                for l in range(len(y_true)):
+                    if pred_or_true == 0 and y_pred[l] == k:           
+                        index = l
+                        target = self.class_name[y_pred[l]]
+                        break
+                    elif pred_or_true == 1 and y_true[l] == k:
+                        index = l
+                        target = self.class_name[y_true[l]]
+                        break
+
+                internal_index = l % 32
+                batch_index = int(l / 32)
+                data_iter = iter(dataloader)
+                current_batch = next(data_iter)
+                for z in range(batch_index):
+                    current_batch = next(data_iter)
+
+                inputs, _ = current_batch
+                inputs = inputs.cpu().detach().numpy()
+                batch_size, seq_len, n_channels, n_samples = inputs.shape
+                inputs = inputs[internal_index].reshape(seq_len, n_samples)
+
+                for j, band in enumerate(band_names):                     
+                    plot_matrix = time_importances_matrix[j][k][internal_index]
+
+                    for a in range(seq_len):
+                        y = []
+                        for b in range(n_samples):
+                            y.append(plot_matrix[a][b])
+
+                        x = np.arange(n_samples)
+
+                        plt.figure(figsize=(15, 5))
+                        plt.subplots_adjust(hspace=0.5)
+
+                        plt.subplot(2, 3, a + 1)           
+                        plt.plot(x, y)
+                        plt.title("Band " + band + " sequence number " + str(a+1) + ": predicted " + self.class_name[y_pred[index]] + ", true " + self.class_name[y_true[index]] + ", importance for " + target)
+
+                        plt.ylabel('Time Importance')
+
+                        x = np.arange(n_samples)
+                        y = inputs[a]
+
+                        plt.subplot(2, 1, a + 4)
+                        plt.plot(x, y)
+                        plt.title("Original corresponding input wave")
+
+                        plt.ylabel("Wave value")
+                        plt.xlabel("Samples")
+
+                    plt.savefig(self.ckpt_path + "band=" + band + "_class=" + class_name + ".png")
+                    plt.close()
+
+                df_current_average = pd.DataFrame(importances_matrix[j], columns = self.class_name)
                 df_current_average["Predicted Label"] = y_pred
                 df_current_average["True Label"] = y_true
                 df_current_average["Fold"] = fold
@@ -508,7 +422,7 @@ class FreqBandsExplainer(PhysioExplainer):
                 if save_csv:
                     df_current_average.to_csv(self.ckpt_path + "band=" + band + "_" + word + "_importance.csv", mode = 'a', index=False)
                 
-        return matrixes_importance
+        return importances_matrix
     
     def explain(self, bands : list[list[float]], band_names : list[str], save_csv : bool = False, plot_pred : bool = False, plot_true : bool = False, n_jobs : int = 10):
 
@@ -517,8 +431,11 @@ class FreqBandsExplainer(PhysioExplainer):
 #        normalized_result = []
 
         # Esegui compute_band_importance per ogni checkpoint in parallelo
-        result = Parallel(n_jobs=n_jobs)(delayed(self.compute_band_importance)(bands, band_names, int(fold), plot_pred, plot_true, save_csv) for fold in self.checkpoints.keys())
-
+        for fold in self.checkpoints.keys():
+            result = self.compute_band_importance(bands, band_names, int(fold), plot_pred, plot_true, save_csv)
+            return result
+        #result = Parallel(n_jobs=n_jobs)(delayed(self.compute_band_importance)(bands, band_names, int(fold), plot_pred, plot_true, save_csv) for fold in self.checkpoints.keys())
+        
         # Converte i risultati in una matrice numpy
 #        simple_result = np.array(simple_result, dtype=object)
 #        weighted_result = np.array(weighted_result, dtype=object)
