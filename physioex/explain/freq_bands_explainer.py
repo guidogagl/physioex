5bb3ec6b6d1e9a7f4738a07a588a426ccb92c690
e-cremente <155981951+e-cremente@users.noreply.github.com>
2024-02-13 15:31:03 +0000

    .

--- physioex/explain/freq_bands_explainer.py
+++ physioex/explain/freq_bands_explainer.py
@@ -409,9 +409,6 @@ class FreqBandsExplainer(PhysioExplainer):
                 for j, band in enumerate(band_names):     
                     logger.info("JOB:%d-Plotting time importance of target band %s for target class %s" % (fold, band, class_name))                
                     plot_matrix = time_importances_matrix[j][batch_index][k][internal_index]
-                    print(plot_matrix)
-                    print(len(plot_matrix))
-                    print(len(plot_matrix[0]))
 
                     fig, axs = plt.subplots(2, 3, figsize=(30, 5))
 
@@ -420,7 +417,7 @@ class FreqBandsExplainer(PhysioExplainer):
                     for a in range(seq_len):
                         y = []
                         for b in range(n_samples):
-                            y.append(plot_matrix[a][b])
+                            y.append(plot_matrix[a][0][b])
 
                         x = np.arange(n_samples)
 
