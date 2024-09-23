7b5bd1ecc73e93119b95e61d8a58773ed16654b4
guidogagl <gagliardi.guido.96@gmail.com>
2024-09-23 10:54:06 +0000

    Refactor code to update method documentation in Preprocessor class

--- physioex/preprocess/preprocessor.py
+++ physioex/preprocess/preprocessor.py
@@ -29,20 +29,21 @@ class Preprocessor:
         """
         Initializes the Preprocessor class.
 
-        Parameters
-        ----------
-        dataset_name : str
-            The name of the dataset to be processed.
-        signal_shape : List[int]
-            A list containing two elements representing the number of channels and the number of timestamps in the signal.
-        preprocessors_name : List[str]
-            A list of names for the preprocessing functions.
-        preprocessors : List[Callable]
-            A list of callable preprocessing functions to be applied to the signals.
-        preprocessors_shape : List[List[int]]
-            A list of shapes corresponding to the output of each preprocessing function.
-        data_folder : str, optional
-            The folder where the dataset is stored. If None, the default data folder is used.
+        Parameters:
+            dataset_name (str): 
+                The name of the dataset to be processed.
+            signal_shape (List[int]): 
+                A list containing two elements representing the number of channels and the number of timestamps in the signal.
+            preprocessors_name (List[str]): 
+                A list of names for the preprocessing functions.
+            preprocessors (List[Callable]): 
+                A list of callable preprocessing functions to be applied to the signals.
+            preprocessors_shape (List[List[int]]): 
+                A list of shapes corresponding to the output of each preprocessing function.
+            data_folder (str, optional): 
+                The folder where the dataset is stored. If None, the default data folder is used.
+            batch_size (int, optional): 
+                The batch size for processing the data. Default is 1000.
         """
 
         assert (
@@ -81,10 +82,8 @@ class Preprocessor:
         Returns a list containing the paths to each subject's record.
         (Required) Method to be implemented by the user.
         
-        Returns
-        -------
-        List[str]
-            A list of paths to each subject's record.
+        Returns:
+            List[str] : A list of paths to each subject's record.
         """
         pass
 
