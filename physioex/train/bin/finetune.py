5fd1203919f9b1b700767f6bf71e94dfc5c722ee
guidogagl <gagliardi.guido.96@gmail.com>
2024-09-25 10:11:39 +0000

    Refactor finetune script CLI documentation and update usage instructions

--- physioex/train/bin/finetune.py
+++ physioex/train/bin/finetune.py
@@ -21,39 +21,41 @@ def finetune_script():
        `$ finetune [PARAMS]`
        
     Args:
-        --model (str, optional): Specify the model to train, can be a yaml file if the model is not registered. Defaults to "chambon2018". If a yaml file is provided, it should contain the model configuration details.
-        `--learning_rate` (float, default: 1e-7): Specify the learning rate for the model.
+        --model (str, optional): Specify the model to train, can be a yaml file if the model is not registered. Defaults to "chambon2018".
+            If a yaml file is provided, it should contain the model configuration details.
+        --learning_rate (float, optional): Specify the learning rate for the model. Defaults to 1e-7.
             Note: A smaller learning rate is often used for fine-tuning to avoid large updates that could disrupt the pre-trained weights.
-        `--checkpoint_path` (str, default: None): Specify the model checkpoint, if None physioex searches into its pretrained models.
+        --checkpoint_path (str, optional): Specify the model checkpoint, if None physioex searches into its pretrained models. Defaults to None.
             Note: Provide the path to a specific checkpoint file to resume training from a saved state.
-        `--checkpoint_dir` (str, default: None): Specify the checkpoint directory where to store the new finetuned model checkpoints.
+        --checkpoint_dir (str, optional): Specify the checkpoint directory where to store the new finetuned model checkpoints. Defaults to None.
             Note: This directory will be used to save checkpoints during training.
-        `--datasets` (list, default: ['mass']): Specify the datasets list to train the model on.
+        --datasets (list, optional): Specify the datasets list to train the model on. Defaults to ['mass'].
             Note: Provide a list of dataset names to be used for training.
-        `--selected_channels` (list, default: ['EEG']): Specify the channels to train the model.
+        --selected_channels (list, optional): Specify the channels to train the model. Defaults to ['EEG'].
             Note: Channels refer to the data modalities (e.g., EEG, EOG) used for training.
-        `--sequence_length` (int, default: 21): Specify the sequence length for the model.
+        --sequence_length (int, optional): Specify the sequence length for the model. Defaults to 21.
             Note: Sequence length refers to the number of time steps in each input sequence.
-        `--loss` (str, default: "cel"): Specify the loss function to use.
+        --loss (str, optional): Specify the loss function to use. Defaults to "cel".
             Note: The loss function determines how the model's performance is measured during training.
-        `--max_epoch` (int, default: 20): Specify the maximum number of epochs for training.
+        --max_epoch (int, optional): Specify the maximum number of epochs for training. Defaults to 20.
             Note: An epoch is one complete pass through the training dataset.
-        `--num_validations` (int, default: 10): Specify the number of validations steps to be done in each epoch.
+        --num_validations (int, optional): Specify the number of validations steps to be done in each epoch. Defaults to 10.
             Note: Validation steps are used to evaluate the model's performance on a validation set during training.
-        `--batch_size` (int, default: 32): Specify the batch size for training.
+        --batch_size (int, optional): Specify the batch size for training. Defaults to 32.
             Note: Batch size refers to the number of samples processed before the model's weights are updated.
-        `--data_folder` (str, optional, default: None): The absolute path of the directory where the physioex dataset are stored, if None the home directory is used.
+        --data_folder (str, optional): The absolute path of the directory where the physioex dataset are stored, if None the home directory is used. Defaults to None.
             Note: Provide the path to the directory containing the datasets.
-        `--test` (bool, optional, default: False): Test the model after training.
+        --test (bool, optional): Test the model after training. Defaults to False.
             Note: If specified, the model will be tested on the validation set after training.
-        `--aggregate` (bool, optional, default: False): Aggregate the results of the test.
+        --aggregate (bool, optional): Aggregate the results of the test. Defaults to False.
             Note: If specified, the test results will be aggregated across multiple datasets.
-        `--hpc` (bool, optional, default: False): Using high performance computing setups or not, need to be called when datasets have been compressed into .h5 format with the compress_datasets command.
+        --hpc (bool, optional): Using high performance computing setups or not, need to be called when datasets have been compressed into .h5 format with the compress_datasets command. Defaults to False.
             Note: Use this option if you are running the script on a high-performance computing cluster.
-        `--num_nodes` (int, default: 1): Specify the number of nodes to be used for distributed training, only used when hpc is True.
+        --num_nodes (int, optional): Specify the number of nodes to be used for distributed training, only used when hpc is True. Defaults to 1.
             Note: In slurm this value needs to be coherent with '--ntasks-per-node' or 'ppn' in torque. This option is relevant for distributed training setups.
-        `--config` (str, default: None): Specify the path to the configuration file where to store the options to train the model with.
+        --config (str, optional): Specify the path to the configuration file where to store the options to train the model with. Defaults to None.
             Note: The configuration file can override command line arguments.
+ 
 
     Example:
         ```bash
