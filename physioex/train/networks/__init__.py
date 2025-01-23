635a72bebbf38645a6a8592bb53a90ded94da959
Guido Gagliardi <guidogagl@users.noreply.github.com>
2025-01-23 14:26:43 +0100

    fixed file path configuration in __init__.py

--- physioex/train/networks/__init__.py
+++ physioex/train/networks/__init__.py
@@ -1,15 +1,14 @@
 import os
 
-import pkg_resources as pkg
+#import pkg_resources as pkg
 import yaml
 
 from physioex.train.networks.chambon2018 import Chambon2018Net
 from physioex.train.networks.seqsleepnet import SeqSleepNet
 from physioex.train.networks.tinysleepnet import TinySleepNet
 
-config_file = pkg.resource_filename(
-    "physioex", os.path.join("train", "networks", "config.yaml")
-)
+config_file = os.path.abspath( os.path.join(os.path.dirname(__file__),"config.yaml") )
+
 if not os.path.exists(config_file):
     raise FileNotFoundError(f"Network configuration file not found: {config_file}")
 
