8a40e9058495e124c9238fcc52d54b9683e6846e
Guido Gagliardi <guido@galatolo.xyz>
2025-03-24 10:48:12 +0000

    update to 1.0.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="physioex",
-    version="1.0.8",
+    version="1.0.9",
     packages=find_packages(),
     install_requires=[
         "boto3",
