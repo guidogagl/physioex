4141a36018dbc092e65e73c8410a0f570c292f12
Javier Garcia <jgciudad@outlook.com>
2025-05-06 16:41:58 +0200

    adapted to channel-specific prototypes

--- physioex/explain/utils/prototypes_latent_space.py
+++ physioex/explain/utils/prototypes_latent_space.py
@@ -25,7 +25,7 @@ from physioex.preprocess.utils.signal import OnlineVariance
 
 def subject_density(prototype_predictions: np.ndarray[np.int_],
                     channels: List[str],
-                    n_proto: int,
+                    n_proto: List[int],
                     subjects: np.ndarray[np.int_],
                     results_path: str):
     
@@ -49,7 +49,7 @@ def subject_density(prototype_predictions: np.ndarray[np.int_],
     
     for c_idx, c in enumerate(channels):
         ratio_table = df.groupby(c)['subjects'].value_counts(normalize=True).unstack().fillna(0)
-        fig, axes = plt.subplots(int(np.ceil(np.sqrt(n_proto))), int(np.floor(np.sqrt(n_proto))), figsize=(25, 25), dpi=300)
+        fig, axes = plt.subplots(int(np.ceil(np.sqrt(n_proto[c_idx]))), int(np.floor(np.sqrt(n_proto[c_idx]))), figsize=(25, 25), dpi=300)
         for i in range(axes.size):
             ax = axes[int(i // axes.shape[1]), int(i % axes.shape[1])]
             if i not in ratio_table.index:
@@ -63,114 +63,76 @@ def subject_density(prototype_predictions: np.ndarray[np.int_],
         plt.savefig(os.path.join(save_path, f'{c}.png'))
         
         ratio_table.plot(kind='bar', stacked=True)
-        plt.savefig(os.path.join(save_path, f'{c}2.png'))
+        plt.savefig(os.path.join(save_path, f'{c}_bars.png'))
 
 
 def count_transitions(prototype_predictions: np.ndarray[np.int_],
                       transition_matrix: np.ndarray[np.int_]):
     
     sequence_center = prototype_predictions.shape[1] // 2 + 1
-    prototype_predictions = prototype_predictions[:, sequence_center-1 : sequence_center+2, :, 0]
+    prototype_predictions = prototype_predictions[:, sequence_center-1 : sequence_center+1, 0]
     
-    for c in range(transition_matrix.shape[0]):
-        for r in prototype_predictions[:, :, c]:
-            transition_matrix[c, r[0], r[1]] += 1
-            transition_matrix[c, r[1], r[2]] += 1
+    for r in prototype_predictions:
+        transition_matrix[r[0], r[1]] += 1
     
     return transition_matrix
         
         
-def average_psd(n_proto: int,
-                channels: List[str],
+def average_psd(channels: List[str],
                 mean_psd: dict,
                 results_path: str) -> None:
     '''Plot the average psd for each prototype and channel'''
     
     n_channels = len(channels)
-    
-    # Create a main grid for each prototype
-    n_rows = int(np.ceil(np.sqrt(n_proto)))  # number of rows in the main grid
-    n_cols = int(np.floor(np.sqrt(n_proto)))  # number of columns in the main grid
-    outer = gridspec.GridSpec(n_rows, n_cols, wspace=0.2, hspace=0.8)
-    outer_linear = gridspec.GridSpec(n_rows, n_cols, wspace=0.2, hspace=0.8)
     cmap = plt.get_cmap("tab10", n_channels)
+
+    for c_idx, c in enumerate(channels):
+        n_proto_c = len(mean_psd[c])
+        fig, axes = plt.subplots(int(np.ceil(np.sqrt(n_proto_c))), int(np.floor(np.sqrt(n_proto_c))), figsize=(5 * int(np.sqrt(n_proto_c)), 4 * int(np.sqrt(n_proto_c))), dpi=300, tight_layout=True)
+        fig_linear, axes_linear = plt.subplots(int(np.ceil(np.sqrt(n_proto_c))), int(np.floor(np.sqrt(n_proto_c))), figsize=(5 * int(np.sqrt(n_proto_c)), 4 * int(np.sqrt(n_proto_c))), dpi=300, tight_layout=True)
+
+        for i in range(axes.size):
+            ax = axes[int(i // axes.shape[1]), int(i % axes.shape[1])]
+            ax_linear = axes_linear[int(i // axes.shape[1]), int(i % axes.shape[1])]
+
+            if i < n_proto_c:
+                s_mean, s_std = mean_psd[c][i].compute()
+                        
+                ax.set_xticks(np.linspace(0, s_mean.size - 1, 11))
+                ax.set_xticklabels(np.arange(0, 51, 5))
+                ax.set_xlim(0, s_mean.size - 1)
+                ax.plot(s_mean, color=cmap(c_idx))
+                ax.fill_between(np.arange(s_mean.size), s_mean - s_std, s_mean + s_std, alpha=0.5, color=cmap(c_idx))
+                ax.set_title(f"P{i} "+ c)
+                ax.set_xlabel("Frequency (Hz)")
+                ax.set_ylabel("PSD (dB)")
+                
+                # Convert s_mean and s_std from dB to linear
+                s_mean = 10 ** (s_mean / 20)
+                s_std = 10 ** (s_std / 20)
+                s_mean = s_mean[:s_mean.size//2]
+                s_std = s_std[:s_std.size//2]
+                ax_linear.set_xticks(np.linspace(0, s_mean.size - 1, 6))
+                ax_linear.set_xticklabels(np.arange(0, 26, 5))
+                ax_linear.set_xlim(0, s_mean.size - 1)
+                ax_linear.plot(s_mean, color=cmap(c_idx))
+                ax_linear.fill_between(np.arange(s_mean.size), s_mean - s_std, s_mean + s_std, alpha=0.5, color=cmap(c_idx))
+                ax_linear.set_title(f"P{i} "+ c)
+                ax_linear.set_xlabel("Frequency (Hz)")
+                ax_linear.set_ylabel("Power (linear)")
+            else:
+                ax.axis('off')
+                ax_linear.axis('off')
+        
+        fig.savefig(os.path.join(results_path, f'{c}_psd_dB.png'))
+        fig_linear.savefig(os.path.join(results_path, f'{c}_psd_linear.png'))
     
-    fig = plt.figure(figsize=(6*(n_cols * n_channels), n_rows * 2), dpi=300)
-    fig_linear = plt.figure(figsize=(6*(n_cols * n_channels), n_rows * 2), dpi=300)
-
-    for i in range(n_rows):
-        for j in range(n_cols):
-            # Compute flat index
-            index = i * n_cols + j
-            
-            if index < n_proto:
-                # Create a 1 x n_channels inner grid inside each cell of the outer grid
-                inner_grid = gridspec.GridSpecFromSubplotSpec(
-                    1, n_channels, subplot_spec=outer[index], wspace=0.3
-                )
-                inner_grid_linear = gridspec.GridSpecFromSubplotSpec(
-                    1, n_channels, subplot_spec=outer_linear[index], wspace=0.3
-                )
-
-                # plot psd for each channel if any prototype is present
-                for k, channel in enumerate(channels):
-                    if mean_psd[index][channel].n > 0:
-                        s_mean, s_std = mean_psd[index][channel].compute()
-                        
-                        ax = plt.Subplot(fig, inner_grid[k])
-                        ax.set_xticks(np.linspace(0, s_mean.size - 1, 11))
-                        ax.set_xticklabels(np.arange(0, 51, 5))
-                        ax.set_xlim(0, s_mean.size - 1)
-                        ax.plot(s_mean, color=cmap(k))
-                        ax.fill_between(np.arange(s_mean.size), s_mean - s_std, s_mean + s_std, alpha=0.5, color=cmap(k))
-                        ax.set_title(f"P{index} "+ channel)
-                        ax.set_xlabel("Frequency (Hz)")
-                        ax.set_ylabel("PSD (dB)")
-                        fig.add_subplot(ax)
-                        
-                        # Convert s_mean and s_std from dB to linear
-                        s_mean = 10 ** (s_mean / 20)
-                        s_std = 10 ** (s_std / 20)
-                        s_mean = s_mean[:s_mean.size//2]
-                        s_std = s_std[:s_std.size//2]
-                        ax = plt.Subplot(fig_linear, inner_grid_linear[k])
-                        ax.set_xticks(np.linspace(0, s_mean.size - 1, 6))
-                        ax.set_xticklabels(np.arange(0, 26, 5))
-                        ax.set_xlim(0, s_mean.size - 1)
-                        ax.plot(s_mean, color=cmap(k))
-                        ax.fill_between(np.arange(s_mean.size), s_mean - s_std, s_mean + s_std, alpha=0.5, color=cmap(k))
-                        ax.set_title(f"P{index} "+ channel)
-                        ax.set_xlabel("Frequency (Hz)")
-                        ax.set_ylabel("Power (linear)")
-                        fig_linear.add_subplot(ax)
-
-    # adjust figure edges
-    fig.subplots_adjust(
-        left=0.03,   # default is 0.125
-        right=0.97,  # default is 0.9
-        top=0.95,    # default is 0.88
-        bottom=0.05, # default is 0.11
-        wspace=0.3,
-        hspace=0.4
-    )
-    fig_linear.subplots_adjust(
-        left=0.03,   # default is 0.125
-        right=0.97,  # default is 0.9
-        top=0.95,    # default is 0.88
-        bottom=0.05, # default is 0.11
-        wspace=0.3,
-        hspace=0.4
-    )
-    fig.savefig(os.path.join(results_path, 'avg_psd_dB.png'))
-    fig_linear.savefig(os.path.join(results_path, 'avg_psd_linear.png'))
-
         
 def prototype_density(prototype_predictions: np.ndarray[np.int_],
+                      n_proto: List[int],
                       y: np.ndarray[np.int_],
                       channels: List[str],
-                      n_proto: int,
                       results_path: str) -> None:
-    
     # Plot the ratio of sleep stage per prototype. One plot per channel. 
     df_dict = {'y': y}
     for c_idx, c in enumerate(channels): 
@@ -179,7 +141,7 @@ def prototype_density(prototype_predictions: np.ndarray[np.int_],
     
     for c_idx, c in enumerate(channels):
         ratio_table = df.groupby(c)['y'].value_counts(normalize=True).unstack().fillna(0)
-        fig, axes = plt.subplots(int(np.ceil(np.sqrt(n_proto))), int(np.floor(np.sqrt(n_proto))), figsize=(25, 25), dpi=300)
+        fig, axes = plt.subplots(int(np.ceil(np.sqrt(n_proto[c_idx]))), int(np.floor(np.sqrt(n_proto[c_idx]))), figsize=(25, 25), dpi=300)
         for i in range(axes.size):
             ax = axes[int(i // axes.shape[1]), int(i % axes.shape[1])]
             if i not in ratio_table.index:
@@ -202,7 +164,7 @@ def prototype_density(prototype_predictions: np.ndarray[np.int_],
         ratio_table = df.groupby('y')[c].value_counts(normalize=True).unstack().fillna(0).T
         ratio_table = ratio_table.reset_index() 
         melted_df = ratio_table.melt(id_vars=c, var_name='Stage', value_name='Sensitivity')
-        for proto in range(n_proto):
+        for proto in range(n_proto[c_idx]):
             if proto not in melted_df[c].unique():
                 melted_df = pd.concat([melted_df, pd.DataFrame({c: [proto], 'Stage': [np.nan], 'Sensitivity': [np.nan]})], ignore_index=True)
         sns.barplot(data=melted_df, x=c, y='Sensitivity', hue='Stage', ax=ax, palette=sns.color_palette("Set1"), hue_order=sorted(melted_df['Stage'].dropna().unique()))
@@ -220,8 +182,8 @@ def prototype_density(prototype_predictions: np.ndarray[np.int_],
     value_counts = {c: df[c].value_counts() for c in channels}
     
     bar_data = []
-    for c in channels:
-        for idx in range(n_proto):
+    for c_idx, c in enumerate(channels):
+        for idx in range(n_proto[c_idx]):
             bar_data.append({'Channel': c, 'Prototype': idx, 'Count': value_counts[c].get(idx, 0)})
     bar_df = pd.DataFrame(bar_data)
     
@@ -256,6 +218,7 @@ def visualize(
     set_float32_matmul_precision("medium")
 
     datamodule_kwargs["batch_size"] = batch_size
+    # datamodule_kwargs["hpc"] = hpc
     datamodule_kwargs["folds"] = fold
     datamodule_kwargs["num_nodes"] = num_nodes
     # datamodule_kwargs["evaluate_on_whole_night"] = True
@@ -312,28 +275,32 @@ def visualize(
     if results_path is not None:
         Path(results_path).mkdir(parents=True, exist_ok=True)
     
-    learned_prototypes = model.nn.prototype.codebook.cpu().detach().numpy()
-    n_proto = learned_prototypes.shape[0]
+    
+    learned_prototypes = [chan_codebook.codebook.cpu().detach().numpy() for chan_codebook in model.nn.prototype]
+    n_proto = [chan_codebook.shape[0] for chan_codebook in learned_prototypes]
     channels = datamodule_kwargs["selected_channels"]
     n_channels = len(channels)
+    assert len(n_proto) == n_channels, "Number of prototypes must match number of channels"
     
     # Create OnlineVariance objects for each prototype and channel
     mean_psd = {}
-    for i in range(n_proto):
-        mean_psd[i] = {}  # initialize inner dict
-        for c in channels:
-            mean_psd[i][c] = OnlineVariance((129,))
+    transition_matrices = {}
+    for c_idx, c in enumerate(channels):
+        mean_psd[c] = {}
+        for i in range(n_proto[c_idx]):
+            mean_psd[c][i] = OnlineVariance((129,))
+        
+        transition_matrices[c] = np.zeros((n_proto[c_idx], n_proto[c_idx]), dtype=np.int_)
     
     # Initialize a dictionary to store embeddings of each channel
     embeddings = {}
     for c in channels:
         embeddings[c] = []
         
-    # Initialize variables to store y, prototype predictions, subject ids and transition matrix
+    # Initialize variables to store y, prototype predictions, subject ids
     y = []
     proto_idx = []
     subjects_id = []
-    transition_matrix = np.zeros((n_channels, n_proto, n_proto), dtype=np.int_)
     
     for _, test_datamodule in enumerate(datamodule):
         dataloader = test_datamodule.test_dataloader(shuffle=True)
@@ -356,7 +323,8 @@ def visualize(
             embeddings_, _, proto_idx_, _, alphas = model.nn.get_prototypes(x_.cuda())
             
             proto_idx_ = proto_idx_.cpu().numpy()
-            transition_matrix = count_transitions(proto_idx_, transition_matrix)
+            for c_idx, c in enumerate(channels):
+                transition_matrices[c] = count_transitions(proto_idx_[:,:,c_idx], transition_matrices[c])
             
             x_ = x_ * test_datamodule.dataset.readers[0].reader.std + test_datamodule.dataset.readers[0].reader.mean
 
@@ -368,16 +336,16 @@ def visualize(
             
             # select time window that was sampled
             sampled_x = np.einsum('bctf, bct -> bcf', x_, alphas)
-
+                        
             # store psd for each prototype and channel
-            for p in np.unique(proto_idx_):
-                for c_idx, c in enumerate(channels):
+            for c_idx, c in enumerate(channels):
+                for p in np.unique(proto_idx_[:, c_idx]):
                     p_filter_idx = np.where(proto_idx_[:, c_idx] == p)[0]
                     
                     if len(p_filter_idx) > 0:
                         signal = sampled_x[p_filter_idx, c_idx]
-                        mean_psd[p][c].add(signal)
-            
+                        mean_psd[c][p].add(signal)
+
             # store embeddings for each channel
             for c_idx, c in enumerate(channels):
                 embeddings[c].append(embeddings_[:,c_idx])
@@ -388,18 +356,19 @@ def visualize(
             proto_idx.append(proto_idx_)
 
         # plot transition matrix
-        transition_matrix = transition_matrix / np.sum(transition_matrix, axis=2, keepdims=True)
-        transition_matrix = np.nan_to_num(transition_matrix, nan=0)
-        fig, axes = plt.subplots(1, n_channels, figsize=(n_channels*n_proto*0.6, n_proto*0.6), dpi=300)
+        for c_idx, c in enumerate(channels):
+            transition_matrices[c] = transition_matrices[c] / np.sum(transition_matrices[c], axis=1, keepdims=True)
+            transition_matrices[c] = np.nan_to_num(transition_matrices[c], nan=0)
+        fig, axes = plt.subplots(1, n_channels, figsize=(n_channels*max(n_proto)*0.6, max(n_proto)*0.6), dpi=300)
         
         for c_idx, c in enumerate(channels):
             if isinstance(axes, np.ndarray):
                 ax = axes[c_idx]
             else:
                 ax = axes
-            sns.heatmap(transition_matrix[c_idx], annot=True, fmt=".2f", cmap="Blues", ax=ax)
-            ax.set_xlabel("Prototype")
-            ax.set_ylabel("Prototype")
+            sns.heatmap(transition_matrices[c], annot=True, fmt=".2f", cmap="Blues", ax=ax)
+            ax.set_xlabel("Prototype n")
+            ax.set_ylabel("Prototype n+1")
             ax.set_title(f"Transition Matrix for {c}")
         plt.tight_layout()
         save_path = Path(results_path) / 'transition_matrix'   
@@ -421,17 +390,17 @@ def visualize(
             y = np.vectorize(label_map.get)(y)
         
         # plot stage ratio in each prototype and prototype counts
-        prototype_density(proto_idx, y, channels, n_proto, results_path)
+        prototype_density(proto_idx, n_proto, y, channels, results_path)
         
         # plot subject density in each prototype
         subject_density(proto_idx, channels, n_proto, subjects_id, results_path)
 
         # plot average psd per prototype
-        average_psd(n_proto, channels, mean_psd, results_path)
+        average_psd(channels, mean_psd, results_path)
 
         # concatenate embeddings and learned prototypes
-        for c in channels:
-            embeddings[c] = np.concatenate((np.squeeze(embeddings[c]), learned_prototypes), axis=0)
+        for c_idx, c in enumerate(channels):
+            embeddings[c] = np.concatenate((np.squeeze(embeddings[c]), learned_prototypes[c_idx]), axis=0)
         
         # plot embeddings and prototypes
         projectors = {'UMAP': UMAP(n_components=2, random_state=42),
@@ -447,10 +416,10 @@ def visualize(
                 axes = np.expand_dims(axes, axis=1)
             for c_idx, c in enumerate(channels):
                 embeddings_2d = v.fit_transform(embeddings[c])
-                sns.scatterplot(ax=axes[0, c_idx], x=embeddings_2d[:-n_proto, 0], y=embeddings_2d[:-n_proto, 1], hue=y, palette=sns.color_palette("bright", len(np.unique(y))))
+                sns.scatterplot(ax=axes[0, c_idx], x=embeddings_2d[:-n_proto[c_idx], 0], y=embeddings_2d[:-n_proto[c_idx], 1], hue=y, palette=sns.color_palette("bright", len(np.unique(y))))
                 
-                for p in range(n_proto):
-                    axes[0, c_idx].text(embeddings_2d[-n_proto+p, 0], embeddings_2d[-n_proto+p, 1], str(p), fontsize=12, weight='bold', color='black', ha='center', va='center')
+                for p in range(n_proto[c_idx]):
+                    axes[0, c_idx].text(embeddings_2d[-n_proto[c_idx]+p, 0], embeddings_2d[-n_proto[c_idx]+p, 1], str(p), fontsize=12, weight='bold', color='black', ha='center', va='center')
                 axes[0, c_idx].set_xlabel(k + " Component 1")
                 axes[0, c_idx].set_ylabel(k + " Component 2")
                 axes[0, c_idx].legend(title="Stage")
@@ -462,9 +431,9 @@ def visualize(
                 axes[0, c_idx].set_xlim((xlim[0], xlim[1]*1.25)) # Restore the original limits and make space for legend
                 axes[0, c_idx].set_ylim(ylim)
                 
-                sns.scatterplot(ax=axes[1, c_idx], x=embeddings_2d[:-n_proto, 0], y=embeddings_2d[:-n_proto, 1], hue=proto_idx[:,c_idx], palette=sns.color_palette("bright", n_proto))
-                for p in range(n_proto):
-                    axes[1, c_idx].text(embeddings_2d[-n_proto+p, 0], embeddings_2d[-n_proto+p, 1], str(p), fontsize=12, weight='bold', color='black', ha='center', va='center')
+                sns.scatterplot(ax=axes[1, c_idx], x=embeddings_2d[:-n_proto[c_idx], 0], y=embeddings_2d[:-n_proto[c_idx], 1], hue=proto_idx[:,c_idx], palette=sns.color_palette("bright", n_proto[c_idx]))
+                for p in range(n_proto[c_idx]):
+                    axes[1, c_idx].text(embeddings_2d[-n_proto[c_idx]+p, 0], embeddings_2d[-n_proto[c_idx]+p, 1], str(p), fontsize=12, weight='bold', color='black', ha='center', va='center')
                 axes[1, c_idx].set_xlabel(k + " Component 1")
                 axes[1, c_idx].set_ylabel(k + " Component 2")
                 axes[1, c_idx].legend(title="Prototype", ncol=2)
